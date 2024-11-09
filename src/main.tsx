e6ef5f01fa35af7603f9cbdf35da92fe5d7e9819
leesiha <mlice1030@soongsil.ac.kr>
2024-11-09 17:38:57 +0900

    fix: update theme import syntax in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App.tsx";
 import { ThemeProvider } from "@emotion/react";
-import theme from "@/styles/theme.ts";
+import { theme } from "@/styles/theme.ts";
 import GlobalStyle from "@/styles/GlobalStyle.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
