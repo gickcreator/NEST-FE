be12a7eb8d9c2330d58b087880ff2daa031d1220
Oliy <eunsoa0310@gmail.com>
2024-11-12 04:46:48 +0900

    fix: develop 충돌 수정

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -4,11 +4,17 @@ import Icn_nav_calendar from '@/assets/svg/icn_nav_calendar.svg?react';
 import Icn_nav_dashboard from '@/assets/svg/icn_nav_dashboard.svg?react';
 import Icn_nav_setting from '@/assets/svg/icn_nav_setting.svg?react';
 import Icn_nav_today from '@/assets/svg/icn_nav_today.svg?react';
-import Arrow_up from '@/assets/svg/arrow-up.svg?react';
-import Refresh from '@/assets/svg/refresh.svg?react';
 import PlusArrow from '@/assets/svg/PlusArrow.svg?react';
 import SelectedBox from '@/assets/svg/Selectbox_Selected.svg?react';
 import UnselectedBox from '@/assets/svg/Selectbox_Unselected.svg?react';
+import DeleteIcon from '@/assets/svg/Delete.svg?react';
+import SettingCheck1 from '@/assets/svg/SettingCheck1Icon.svg?react';
+import SettingCheck2 from '@/assets/svg/SettingCheck2Icon.svg?react';
+import SettingCheck3 from '@/assets/svg/SettingCheck3Icon.svg?react';
+import SettingProgress from '@/assets/svg/SettingProgressIcon.svg?react';
+import SettingX from '@/assets/svg/SettingXIcon.svg?react';
+import Arrow_up from '@/assets/svg/arrow-up.svg?react';
+import Refresh from '@/assets/svg/refresh.svg?react';
 
 const Icons = {
     Icn_clock,
@@ -24,6 +30,12 @@ const Icons = {
     SelectedBox,
 	UnselectedBox,
     PlusArrow,
+    DeleteIcon,
+	SettingCheck1,
+	SettingCheck2,
+	SettingCheck3,
+	SettingX,
+	SettingProgress,
 };
 
 export default Icons;
