2ed82092ee6bfc13f98b9a43200f1ad71b13648a
leesiha <mlice1030@soongsil.ac.kr>
2024-11-19 00:58:20 +0900

    feat: add storybook
    - 스토리북 설정 및 버튼 컴포넌트 추가

---
+++ src/stories/Button/Button.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+import buttonStyles from './buttonStyle';
+
+export interface ButtonProps {
+    primary?: boolean;
+    backgroundColor?: string;
+    size?: 'small' | 'medium' | 'large';
+    label: string;
+    onClick?: () => void;
+}
+
+const StyledButton = styled.button`
+	${buttonStyles}
+`;
+
+function Button({
+    primary = false,
+    backgroundColor,
+    size = 'medium',
+    label = 'button',
+    onClick = () => { },
+}: ButtonProps) {
+    const mode = primary ? 'storybook-button-primary' : 'storybook-button-secondary';
+    const sizeClass = `storybook-button-${size}`;
+    return (
+        <StyledButton className={`${mode} ${sizeClass}`} style={{ backgroundColor }} onClick={onClick}>
+            {label}
+        </StyledButton>
+    );
+}
+
+export default Button;
