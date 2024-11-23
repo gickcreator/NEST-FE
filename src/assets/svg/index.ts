807398e65438511a43eb874bde77cfa670e3c9e9
Siha Lee <mlice1030@soongsil.ac.kr>
2024-11-23 13:59:11 +0900

    Feature/common component button publishing (#15)
    * fix: button 태그 기본 스타일 초기화
    
    * feat: 텍스트 btn 퍼블리싱
    
    * feat: 동기화 btn 퍼블리싱
    
    * feat: 삭제/취소 btn 퍼블리싱
    
    * feat: icon import
    
    * fix: interface 컨벤션 반영
    
    * feat: 오늘로 추가 버튼 구현
    
    * fix: develop 충돌 수정
    
    * fix: develop 충돌 수정
    
    * fix: develop 충돌 수정
    
    * fix: setting progress 아이콘 prop 분리
    
    * feat: timeline delete 버튼 구현
    
    * feat: Status 버튼 구현
    
    * feat: 확인/취소 버튼 구현
    
    * fix: theme 적용
    
    * fix: svg 크기 & 정렬 수정
    
    * fix: svg color 적용
    
    * fix: rem 단위 수정
    
    * fix: disabled 속성 적용
    
    * fix: light 처리
    
    ---------
    
    Co-authored-by: Oliy <eunsoa0310@gmail.com>

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,18 @@
+import DeleteIcon from '@/assets/svg/Delete.svg?react';
+import DoneIcon from '@/assets/svg/DoneIcon.svg?react';
+import PlusArrow from '@/assets/svg/PlusArrow.svg?react';
+import ProgressIcon from '@/assets/svg/ProgressIcon.svg?react';
+import SelectedBox from '@/assets/svg/Selectbox_Selected.svg?react';
+import UnselectedBox from '@/assets/svg/Selectbox_Unselected.svg?react';
+import SettingCheck2 from '@/assets/svg/SettingCheck2Icon.svg?react';
+import SettingCheck3 from '@/assets/svg/SettingCheck3Icon.svg?react';
+import SettingProgress from '@/assets/svg/SettingProgressIcon.svg?react';
+import SettingX from '@/assets/svg/SettingXIcon.svg?react';
+import Arrow_up from '@/assets/svg/arrow-up.svg?react';
+import TimelineDelete from '@/assets/svg/ic_delete.svg?react';
+import Refresh from '@/assets/svg/refresh.svg?react';
+import SettingCheck1 from '@/assets/svg/SettingCheck1.svg?react';
+
 import Icn_arrow_narrow_right from '@/assets/svg/arrow-narrow-right.svg?react';
 import Icn_calander from '@/assets/svg/calendar-minus-01.svg?react';
 import Icn_date_clock from '@/assets/svg/clock.svg?react';
@@ -22,6 +37,20 @@ const Icons = {
         Icn_nav_setting,
         Icn_nav_today,
     },
+    Arrow_up,
+    Refresh,
+    SelectedBox,
+	UnselectedBox,
+    PlusArrow,
+    DeleteIcon,
+	SettingCheck1,
+	SettingCheck2,
+	SettingCheck3,
+	SettingX,
+	SettingProgress,
+    DoneIcon,
+    ProgressIcon,
+    TimelineDelete,
 };
 
 export default Icons;
