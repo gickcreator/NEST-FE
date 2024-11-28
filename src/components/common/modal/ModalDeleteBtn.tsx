9cade64e6728ae698bc75922770e975abb44434c
Oliy <eunsoa0310@gmail.com>
2024-11-28 16:32:44 +0900

    fix : Task 할당 모달 스타일 수정

--- src/components/common/modal/ModalDeleteBtn.tsx
+++ src/components/common/modal/ModalDeleteBtn.tsx
@@ -5,20 +5,28 @@ import SettingDeleteBtn from '@/components/common/button/settingBtn/SettingDelet
 import ModalDeleteDetail from '@/components/common/modal/ModalDeleteDetail';
 
 function ModalDeleteBtn() {
-    const [isClicked, setIsClicked] = useState(false);
-    const handleBtnClick = () => {
-        setIsClicked((prev) => !prev);
-    };
-    return (
-        <ModalDeleteBtnLayout>
-            <SettingDeleteBtn size="big" isHover isPressed={false} isActive onClick={handleBtnClick} />
-            {isClicked && (
-                <ModalDeleteDetailWapper>
-                    <ModalDeleteDetail />
-                </ModalDeleteDetailWapper>
-            )}
-        </ModalDeleteBtnLayout>
-    );
+	const [isClicked, setIsClicked] = useState(false);
+
+	const [top, setTop] = useState(0);
+	const [left, setLeft] = useState(0);
+
+	const handleBtnClick = (e: React.MouseEvent) => {
+		const rect = e.currentTarget.getBoundingClientRect();
+		setTop(rect.bottom + 6);
+		setLeft(rect.left);
+		setIsClicked((prev) => !prev);
+	};
+
+	return (
+		<ModalDeleteBtnLayout>
+			<SettingDeleteBtn size="big" isHover isPressed={false} isActive onClick={handleBtnClick} />
+			{isClicked && (
+				<ModalDeleteDetailWapper>
+					<ModalDeleteDetail top={top} left={left} onClose={handleBtnClick} />
+				</ModalDeleteDetailWapper>
+			)}
+		</ModalDeleteBtnLayout>
+	);
 }
 
 const ModalDeleteBtnLayout = styled.div`
