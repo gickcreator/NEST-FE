9659850747cb253562f42276cf8425fd6c910592
leesiha <mlice1030@soongsil.ac.kr>
2024-12-02 16:23:48 +0900

    feat : Google 로그인 기능 추가 및 axios 인스턴스 설정
     - 현재 백엔드 서버 배포(도메인)가 완료되지 않아, 임시로 목데이터를 넣어두었습니다.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@emotion/react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App.tsx';
@@ -15,12 +16,14 @@ const CLIENT_ID = import.meta.env.VITE_GOOGLE_LOGIN_CLIENT_ID;
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<GoogleOAuthProvider clientId={CLIENT_ID}>
-		<QueryClientProvider client={queryClient}>
-			<GlobalStyle />
-			<ThemeProvider theme={theme}>
-				<App />
-			</ThemeProvider>
-			<ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" position="left" />
-		</QueryClientProvider>
+		<React.StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<GlobalStyle />
+				<ThemeProvider theme={theme}>
+					<App />
+				</ThemeProvider>
+				<ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" position="left" />
+			</QueryClientProvider>
+		</React.StrictMode>
 	</GoogleOAuthProvider>
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,44 @@
+import styled from '@emotion/styled';
+import { useGoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+
+import userLogin from '@/apis/login/loginAxios';
+
 function Login() {
-	return <>login</>;
+	const navigate = useNavigate();
+
+	
+	const googleSocialLogin = () => {
+		// 목데이터로 임시 로그인
+		localStorage.setItem('accessToken', 'accessToken');
+		localStorage.setItem('refreshToken', 'refreshToken');
+		navigate('/today');
+	}
+	// useGoogleLogin({
+	// 	onSuccess: async ({ code }) => {
+	// 		try {
+	// 			const response = await userLogin(code);
+	// 			if (response.code === 'success') {
+	// 				localStorage.setItem('accessToken', response.data.accessToken);
+	// 				localStorage.setItem('refreshToken', response.data.refreshToken);
+	// 				navigate('/today');
+	// 			}
+	// 		} catch (error) {
+	// 			console.error(error);
+	// 		}
+	// 	},
+	// 	onError: (errorResponse) => {
+	// 		console.error(errorResponse);
+	// 	},
+	// 	flow: 'auth-code',
+	// 	scope: 'email profile',
+	// });
+
+	return <GoogleBtn onClick={googleSocialLogin}>Google Button</GoogleBtn>;
 }
+
+const GoogleBtn = styled.button`
+	width: 30rem;
+`;
 
 export default Login;
