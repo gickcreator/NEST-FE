1b3fa16c075012ee6f815babcd73aa804a91152f
leesiha <mlice1030@soongsil.ac.kr>
2024-12-15 14:30:54 +0900

    feat : TargetArea 컴포넌트에서 날짜 형식을 문자열로 변경하고 관련 기능 수정

--- src/components/targetArea/TargetTaskSection.tsx
+++ src/components/targetArea/TargetTaskSection.tsx
@@ -1,81 +1,52 @@
+import { Draggable } from 'react-beautiful-dnd';
+
 import BtnTask from '../common/BtnTask/BtnTask';
 import BtnTaskContainer from '../common/BtnTaskContainer';
-import ScrollGradient from '../common/ScrollGradient';
 
+import useGetTasks from '@/apis/tasks/getTask/query';
 import { TaskType } from '@/types/tasks/taskType';
+import formatDatetoLocalDate from '@/utils/formatDatetoLocalDate';
 
 interface TargetTaskSectionProps {
 	handleSelectedTarget: (task: TaskType | null) => void;
 	selectedTarget: TaskType | null;
-	tasks?: TaskType[]; // 선택적 속성으로 유지
+	selectedDate: Date | null;
 }
-
-function TargetTaskSection({
-	handleSelectedTarget,
-	selectedTarget,
-	tasks,
-}: TargetTaskSectionProps) {
-	// tasks가 없을 경우 dummyTaskList를 기본값으로 사용
-	const taskList = tasks ?? [
-		{
-			id: 0,
-			name: '밥먹기',
-			deadLine: {
-				date: '2024-11-30',
-				time: '12:30',
-			},
-			hasDescription: false,
-			status: '진행중',
-		},
-		{
-			id: 1,
-			name: '오소기 발표 준비',
-			deadLine: {
-				date: '2024-12-01',
-				time: '12:30',
-			},
-			hasDescription: true,
-			status: '지연',
-		},
-		{
-			id: 2,
-			name: '이시하',
-			deadLine: {
-				date: '2024-12-02',
-				time: '12:30',
-			},
-			hasDescription: false,
-			status: '완료',
-		},
-		{
-			id: 3,
-			name: '조영주',
-			deadLine: {
-				date: '2024-12-03',
-				time: '12:30',
-			},
-			hasDescription: true,
-			status: '미완료',
-		},
-	];
-
+function TargetTaskSection(props: TargetTaskSectionProps) {
+	const { handleSelectedTarget, selectedTarget, selectedDate } = props;
+	const targetDate = formatDatetoLocalDate(selectedDate);
+	const { isFetched, data } = useGetTasks({ targetDate });
 	return (
 		<BtnTaskContainer type="target">
-			{taskList.map((task) => (
-				<BtnTask
-					iconType="active"
-					key={task.id}
-					hasDescription={task.hasDescription}
-					name={task.name}
-					deadLine={task.deadLine}
-					status={task.status}
-					id={task.id}
-					handleSelectedTarget={handleSelectedTarget}
-					selectedTarget={selectedTarget}
-					preventDoubleClick={false}
-				/>
-			))}
-			<ScrollGradient />
+			{isFetched && (
+				<>
+					{data.data.tasks.map((task: TaskType, index: number) => (
+						<Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+							{(provided, snapshot) => (
+								<div
+									ref={provided.innerRef}
+									{...provided.draggableProps}
+									{...provided.dragHandleProps}
+									style={{ userSelect: 'none', ...provided.draggableProps.style }}
+								>
+									<BtnTask
+										iconType="active"
+										key={task.id}
+										hasDescription={task.hasDescription}
+										name={task.name}
+										deadLine={task.deadLine}
+										status={task.status}
+										id={task.id}
+										handleSelectedTarget={handleSelectedTarget}
+										selectedTarget={selectedTarget}
+										isDragging={snapshot.isDragging}
+									/>
+								</div>
+							)}
+						</Draggable>
+					))}
+				</>
+			)}
 		</BtnTaskContainer>
 	);
 }
