b8ea99756ab1c8e247814ce39c6b2a1ea72ff491
leesiha <mlice1030@soongsil.ac.kr>
2024-12-15 12:52:57 +0900

    feat : 구글 로그인 버튼 컴포넌트로 변경 및 관련 코드 정리

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,50 +1,15 @@
 import styled from '@emotion/styled';
-import { useGoogleLogin } from '@react-oauth/google';
-import { useNavigate } from 'react-router-dom';
 
-import userLogin from '@/apis/login/loginAxios';
 import Images from '@/assets/images';
+import GoogleLoginBtn from '@/components/loginPage/GoogleLoginBtn';
 
 function Login() {
-	const navigate = useNavigate();
-
-
-	const googleSocialLogin = 
-	// () => {
-		// 목데이터로 임시 로그인
-		// localStorage.setItem('accessToken', 'accessToken');
-		// localStorage.setItem('refreshToken', 'refreshToken');
-		// navigate('/today');
-		// }
-		useGoogleLogin({
-			onSuccess: async ({ code }) => {
-				try {
-					const response = await userLogin(code);
-					if (response.code === 'success') {
-						localStorage.setItem('accessToken', response.data.accessToken);
-						localStorage.setItem('refreshToken', response.data.refreshToken);
-						navigate('/today');
-					}
-				} catch (error) {
-					console.error(error);
-				}
-			},
-			onError: (errorResponse) => {
-				console.error(errorResponse);
-			},
-			flow: 'auth-code',
-			scope: 'email profile',
-		});
-
 	return (
 		<LoginLayout>
 			<LeftSection>
 				<LogoTitleImg src={Images.titleIcon} />
 				<LoginBtn>
-					<GoogleBtn onClick={googleSocialLogin}>
-						<GoogleImg src={Images.googleIcon} />
-						구글 계정으로 시작하기
-					</GoogleBtn>
+					<GoogleLoginBtn />
 					<SignDescription>
 						가입하면 자동으로 <a href="https://github.com/TEAM-DAWM/NUTSHELL-FE/pull/146">개인정보보호정책</a>과{' '}
 						<a href="https://github.com/TEAM-DAWM/NUTSHELL-FE">이용약관</a>에 동의한 것으로 간주됩니다.
@@ -92,32 +57,15 @@ const LoginBtn = styled.div`
 	height: 9.5rem;
 `;
 
-const GoogleBtn = styled.button`
-	display: flex;
-	gap: 0.9rem;
-	align-items: center;
-	justify-content: center;
-	width: 100%;
-	height: 4.3rem;
-	color: ${({ theme }) => theme.palette.Grey.White};
-	background-color: ${({ theme }) => theme.palette.Grey.Black};
-	border-radius: 12px;
-	${({ theme }) => theme.fontTheme.LABEL_03};
-`;
-
-const GoogleImg = styled.img`
-	width: 1.8rem;
-	height: 1.8rem;
-`;
-
 const SignDescription = styled.p`
 	width: 24rem;
+
 	color: ${({ theme }) => theme.palette.Grey.Grey5};
 	text-align: center;
+
 	${({ theme }) => theme.fontTheme.CAPTION_02};
 	a {
 		color: ${({ theme }) => theme.palette.Grey.Grey5};
 	}
 `;
-
 export default Login;
